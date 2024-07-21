ea766cfcf75eb271823259647f4a885b04ba754c
idealflower-k <aekrcu@naver.com>
2024-07-21 19:47:20 +0900

    KL-66/feat: create user repository
    - User의 Repository를 구현했습니다.

---
+++ src/main/java/taco/klkl/domain/user/dao/UserRepository.java
@@ -0,0 +1,10 @@
+package taco.klkl.domain.user.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import taco.klkl.domain.user.domain.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
