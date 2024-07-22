7ce810525789b29e07344ec512603c54728f61ad
ohhamma <judyham10634@naver.com>
2024-07-22 18:07:56 +0900

    KL-100/chore: add dummy data
    - 임시 User 객체 생성
    - 도메인별 더미 객체 생성 구역 나누기

---
+++ src/main/resources/database/data.sql
@@ -0,0 +1,17 @@
+/* User */
+INSERT INTO User(user_id, profile, name, gender, age, description, created_at)
+VALUES (1, 'image/test.jpg', 'testUser', '남', 20, '테스트입니다.', now());
+
+/* Product */
+
+/* Like */
+
+/* Comment */
+
+/* Region */
+
+/* Category */
+
+/* Filter */
+
+/* Notification */
