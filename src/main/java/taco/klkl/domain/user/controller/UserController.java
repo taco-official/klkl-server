3f34fd4d9e909c80c8df535d7322d34dbebc0100
ohhamma <judyham10634@naver.com>
2024-07-22 18:06:49 +0900

    KL-100/chore: add swagger annotations
    - RequestMapping에서 'api' 제거
    - user-controller 태그 추가
    - /me 경로 설명 추가

--- src/main/java/taco/klkl/domain/user/controller/UserController.java
+++ src/main/java/taco/klkl/domain/user/controller/UserController.java
@@ -6,13 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import taco.klkl.domain.user.domain.User;
 import taco.klkl.domain.user.service.UserService;
 
 @Slf4j
 @RestController
-@RequestMapping("api/v1/users")
+@Tag(name = "1. 유저", description = "유저 관련 API")
+@RequestMapping("/v1/users")
 public class UserController {
 	final UserService userService;
 
@@ -20,6 +23,7 @@ public class UserController {
 		this.userService = userService;
 	}
 
+	@Operation(summary = "내 정보 조회", description = "내 정보를 조회합니다. (테스트용)")
 	@GetMapping("/me")
 	public ResponseEntity<User> getMe() {
 		User user = userService.me();
