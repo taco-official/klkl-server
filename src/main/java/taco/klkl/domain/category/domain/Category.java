489d166fa918496f7404b061ec55f1c63ec66076
youngmin <youngmin@youngminui-MacBookPro.local>
2024-07-25 12:30:37 +0900

    KL-69/feat: create category entity
    ID, 카테고리 이름이 들어간 category entity 구현.

---
+++ src/main/java/taco/klkl/domain/category/domain/Category.java
@@ -0,0 +1,33 @@
+package taco.klkl.domain.category.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Category {
+	@Id
+	@Column(name = "category_id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(name = "name", nullable = false, unique = true, length = 20)
+	private String name;
+
+	private Category(String name) {
+		this.name = name;
+	}
+
+	public static Category of(String name) {
+		return new Category(name);
+	}
+}
