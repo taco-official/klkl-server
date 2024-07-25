6523d941d35b9ebe30a2fb1c211a39914d601667
idealflower-k <aekrcu@naver.com>
2024-07-25 19:09:57 +0900

    KL-118/feat: create region entity

---
+++ src/main/java/taco/klkl/domain/region/domain/Region.java
@@ -0,0 +1,32 @@
+package taco.klkl.domain.region.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Region {
+
+	@Id
+	@Column(name = "region_id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	Long regionId;
+
+	@Column(name = "name", length = 20, nullable = false)
+	String name;
+
+	private Region(String name) {
+		this.name = name;
+	}
+
+	public Region of(String name) {
+		return new Region(name);
+	}
+}
