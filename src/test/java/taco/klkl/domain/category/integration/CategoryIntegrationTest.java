8c0de6139ab4c5a93f078b915ab1ae5a844b33fd
youngmin <youngmin@youngminui-MacBookPro.local>
2024-07-25 13:03:19 +0900

    KL-69/create: create CategoryIntegrationTest
    카테고리 API호출시 전체적인 결과를 테스트하는 테스트 코드를 구현.

---
+++ src/test/java/taco/klkl/domain/category/integration/CategoryIntegrationTest.java
@@ -0,0 +1,50 @@
+package taco.klkl.domain.category.integration;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import jakarta.transaction.Transactional;
+import taco.klkl.domain.category.dao.CategoryRepository;
+import taco.klkl.domain.category.dto.response.CategoryResponse;
+import taco.klkl.domain.category.service.CategoryService;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+class CategoryIntegrationTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private CategoryService categoryService;
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+
+	@Test
+	@DisplayName("카테고리 목록 반환 API 통합 Test")
+	void testGetAllCategories() throws Exception {
+		// given
+		List<CategoryResponse> categoryResponses = categoryService.getCategories();
+
+		//then
+		mockMvc.perform(get("/v1/categories")
+				.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.data", hasSize(categoryResponses.size())))
+			.andExpect(jsonPath("$.isSuccess", is(true)))
+			.andExpect(jsonPath("$.code", is("C000")));
+	}
+}
