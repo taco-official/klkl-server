34f6f5b7afb5aeb5dabe9cc2d34948a897222910
ohhamma <judyham10634@naver.com>
2024-07-26 16:11:42 +0900

    KL-110/feat: modify ProductSimpleResponseDto
    - 불필요한 필드 제거
    - 불필요한 NotNull 제거
    - likeCount 필드 타입 변경
    - 정적 팩토리 메서드 수정

--- src/main/java/taco/klkl/domain/product/dto/response/ProductSimpleResponseDto.java
+++ src/main/java/taco/klkl/domain/product/dto/response/ProductSimpleResponseDto.java
@@ -1,6 +1,5 @@
 package taco.klkl.domain.product.dto.response;
 
-import jakarta.validation.constraints.NotNull;
 import taco.klkl.domain.product.domain.Product;
 
 /**
@@ -8,31 +7,25 @@ import taco.klkl.domain.product.domain.Product;
  * TODO: 상품필터속성 추가 해야함 (상품필터속성 테이블 개발 후)
  * @param productId
  * @param name
- * @param description
  * @param likeCount
  * @param cityId
  * @param subcategoryId
- * @param currencyId
  */
 public record ProductSimpleResponseDto(
-	@NotNull(message = "productId is required") Long productId,
-	@NotNull(message = "name is required") String name,
-	@NotNull(message = "description is required") String description,
-	@NotNull(message = "likeCount is required") Long likeCount,
-	@NotNull(message = "cityId is required") Long cityId,
-	@NotNull(message = "subcategoryId is required") Long subcategoryId,
-	@NotNull(message = "currencyId is required") Long currencyId
+	Long productId,
+	String name,
+	int likeCount,
+	Long cityId,
+	Long subcategoryId
 ) {
 
-	public ProductSimpleResponseDto from(Product product) {
+	public static ProductSimpleResponseDto from(Product product) {
 		return new ProductSimpleResponseDto(
 			product.getProductId(),
 			product.getName(),
-			product.getDescription(),
 			product.getLikeCount(),
 			product.getCityId(),
-			product.getSubcategoryId(),
-			product.getCurrencyId()
+			product.getSubcategoryId()
 		);
 	}
 }
