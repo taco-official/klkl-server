fcdb2280c522b1614a66b2f0bb8f6745fe339573
ohhamma <judyham10634@naver.com>
2024-07-26 18:42:37 +0900

    KL-110/feat: create product and get product info
    - ProductService 인터페이스 삭제 및 클래스로 변환
    - Product 생성
    - id로 Product의 상세 정보 조회

--- src/main/java/taco/klkl/domain/product/service/ProductService.java
+++ src/main/java/taco/klkl/domain/product/service/ProductService.java
@@ -1,22 +1,49 @@
 package taco.klkl.domain.product.service;
 
-import java.util.List;
-
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import lombok.RequiredArgsConstructor;
+import taco.klkl.domain.product.dao.ProductRepository;
 import taco.klkl.domain.product.domain.Product;
 import taco.klkl.domain.product.dto.request.ProductCreateRequestDto;
-import taco.klkl.domain.product.dto.request.ProductUpdateRequestDto;
+import taco.klkl.domain.product.dto.response.ProductDetailResponseDto;
+import taco.klkl.domain.product.exception.ProductNotFoundException;
+import taco.klkl.domain.user.domain.User;
+import taco.klkl.global.util.UserUtil;
 
 @Service
-public interface ProductService {
-	Product createProduct(ProductCreateRequestDto productCreateRequestDto);
+@Transactional
+@RequiredArgsConstructor
+public class ProductService {
 
-	Product getProductById(Long id);
+	private final ProductRepository productRepository;
+	private final UserUtil userUtil;
 
-	List<Product> getAllProducts();
+	public ProductDetailResponseDto getProductInfoById(Long id) {
+		Product product = productRepository.findById(id)
+			.orElseThrow(ProductNotFoundException::new);
+		return ProductDetailResponseDto.from(product);
+	}
 
-	Product updateProduct(Long id, ProductUpdateRequestDto productUpdateRequestDto);
+	public ProductDetailResponseDto createProduct(final ProductCreateRequestDto productDto) {
+		final Product product = createProductEntity(productDto);
+		productRepository.save(product);
+		return ProductDetailResponseDto.from(product);
+	}
 
-	void deleteProduct(Long id);
+	private Product createProductEntity(final ProductCreateRequestDto productDto) {
+		final User user = userUtil.findTestUser();
+		return Product.of(
+			user,
+			productDto.name(),
+			productDto.description(),
+			productDto.address(),
+			productDto.price(),
+			productDto.cityId(),
+			productDto.subcategoryId(),
+			productDto.currencyId()
+		);
+	}
+
 }
