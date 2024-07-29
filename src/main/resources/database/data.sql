150afb91eb9e568864bf4773f678e0bc96382a09
sanghwa <77483110+idealflower-k@users.noreply.github.com>
2024-07-29 14:26:35 +0900

    Revert "feat(KL-118): 지역 api 구현"

--- src/main/resources/database/data.sql
+++ src/main/resources/database/data.sql
@@ -9,14 +9,9 @@ VALUES (1, 'image/test.jpg', 'testUser', '남', 20, '테스트입니다.', now()
 /* Comment */
 
 /* Region */
-INSERT INTO Region(region_id, name)
-VALUES (400, '동북아시아'),
-       (401, '동남아시아'),
-       (402, '기타');
 
 /* Category */
-INSERT
-INTO Category(category_id, name)
+INSERT INTO Category(category_id, name)
 VALUES (300, '식품'),
        (301, '의류'),
        (302, '잡화'),
