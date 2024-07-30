c8ad52be98a38a1ba8a4680f5e988731081986e3
youngmin <youngmin@youngminui-MacBookPro.local>
2024-07-30 14:13:04 +0900

    Kl-97/feat : add subcategory enum converter
    enum converter를 구현하여 DB에 enum값이 저장될 경우 string으로 변환하도록 구현.

---
+++ src/main/java/taco/klkl/domain/subcategory/convert/SubcategoryNameConverter.java
@@ -0,0 +1,22 @@
+package taco.klkl.domain.subcategory.convert;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+import taco.klkl.domain.subcategory.domain.SubcategoryName;
+
+@Converter(autoApply = true)
+public class SubcategoryNameConverter implements AttributeConverter<SubcategoryName, String> {
+
+	@Override
+	public String convertToDatabaseColumn(SubcategoryName subcategoryName) {
+		if (subcategoryName == null) {
+			return null;
+		}
+		return subcategoryName.getName();
+	}
+
+	@Override
+	public SubcategoryName convertToEntityAttribute(String name) {
+		return SubcategoryName.getByName(name);
+	}
+}
