b8652919f28f81cb6bf443255a9276dbcaa4cacb
idealflower-k <aekrcu@naver.com>
2024-07-30 18:11:41 +0900

    KL-83/feat: create country not found exception

---
+++ src/main/java/taco/klkl/domain/region/exception/CountryNotFoundException.java
@@ -0,0 +1,11 @@
+package taco.klkl.domain.region.exception;
+
+import taco.klkl.global.error.exception.CustomException;
+import taco.klkl.global.error.exception.ErrorCode;
+
+public class CountryNotFoundException extends CustomException {
+
+	public CountryNotFoundException() {
+		super(ErrorCode.COUNTRY_NOT_FOUND);
+	}
+}
