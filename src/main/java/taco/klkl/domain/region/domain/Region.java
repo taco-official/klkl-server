aa869f51278ffb08020ba635c1f73546dbef7c22
idealflower-k <aekrcu@naver.com>
2024-07-30 18:04:22 +0900

    KL-83/feat: add one to many with country

--- src/main/java/taco/klkl/domain/region/domain/Region.java
+++ src/main/java/taco/klkl/domain/region/domain/Region.java
@@ -1,10 +1,15 @@
 package taco.klkl.domain.region.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,6 +27,9 @@ public class Region {
 
 	@Column(name = "name", length = 20, nullable = false)
 	RegionType name;
+
+	@OneToMany(mappedBy = "region", fetch = FetchType.LAZY, orphanRemoval = true)
+	List<Country> countries = new ArrayList<>();
 
 	private Region(RegionType region) {
 		this.name = region;
