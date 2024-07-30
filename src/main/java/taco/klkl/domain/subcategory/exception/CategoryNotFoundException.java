f867f120bef12dffe529f177874a37d169d2c4a4
youngmin <youngmin@youngminui-MacBookPro.local>
2024-07-30 14:14:10 +0900

    KL-87/feat : add categorynotfoundexception
    Custom Exception을 상속 받은 CategoryNotFoundException을 구현.

---
+++ src/main/java/taco/klkl/domain/subcategory/exception/CategoryNotFoundException.java
@@ -0,0 +1,10 @@
+package taco.klkl.domain.subcategory.exception;
+
+import taco.klkl.global.error.exception.CustomException;
+import taco.klkl.global.error.exception.ErrorCode;
+
+public class CategoryNotFoundException extends CustomException {
+	public CategoryNotFoundException() {
+		super(ErrorCode.CATEGORY_ID_NOT_FOUND);
+	}
+}
