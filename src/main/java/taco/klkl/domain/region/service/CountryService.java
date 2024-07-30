5518e5129fcf4ae24e6246f09ba239d841f9c587
idealflower-k <aekrcu@naver.com>
2024-07-30 18:12:50 +0900

    KL-83/feat: create country service interface

---
+++ src/main/java/taco/klkl/domain/region/service/CountryService.java
@@ -0,0 +1,18 @@
+package taco.klkl.domain.region.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import taco.klkl.domain.region.dto.response.CountryResponseDto;
+import taco.klkl.domain.region.enums.CountryType;
+
+@Service
+public interface CountryService {
+	List<CountryResponseDto> getALlCountry();
+
+	CountryResponseDto getCountryById(Long countryId);
+
+	CountryResponseDto getCountryByName(CountryType name);
+
+}
