31f34a1b5e484bcca3211855b2b91704fdbe5077
idealflower-k <aekrcu@naver.com>
2024-07-31 13:54:14 +0900

    KL-83/refactor: remove country code in enum

--- src/main/java/taco/klkl/domain/region/enums/CountryType.java
+++ src/main/java/taco/klkl/domain/region/enums/CountryType.java
@@ -9,50 +9,42 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum CountryType {
 	// 일본
-	JAPAN(392, "일본"),
+	JAPAN("일본"),
 
 	// 중국
-	CHINA(156, "중국"),
+	CHINA("중국"),
 
 	// 대만
-	TAIWAN(158, "대만"),
+	TAIWAN("대만"),
 
 	// 태국
-	THAILAND(764, "태국"),
+	THAILAND("태국"),
 
 	// 베트남
-	VIETNAM(704, "베트남"),
+	VIETNAM("베트남"),
 
 	// 필리핀
-	PHILIPPINES(608, "필리핀"),
+	PHILIPPINES("필리핀"),
 
 	// 싱가포르
-	SINGAPORE(702, "싱가포르"),
+	SINGAPORE("싱가포르"),
 
 	// 인도네시아
-	INDONESIA(360, "인도네시아"),
+	INDONESIA("인도네시아"),
 
 	// 말레이시아
-	MALAYSIA(458, "말레이시아"),
+	MALAYSIA("말레이시아"),
 
 	// 괌
-	GUAM(316, "괌"),
+	GUAM("괌"),
 
 	// 미국
-	USA(840, "미국"),
+	USA("미국"),
 
 	// NONE
-	NONE(0, "");
+	NONE("");
 
-	private final long countryCode;
 	private final String name;
-
-	public static CountryType getCountryTypeByCode(int countryCode) {
-		return Arrays.stream(CountryType.values())
-			.filter(c -> c.getCountryCode() == countryCode)
-			.findFirst()
-			.orElse(NONE);
-	}
 
 	public static CountryType getCountryTypeByName(String name) {
 		return Arrays.stream(CountryType.values())
