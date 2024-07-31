201f2b62c92a19bd090191b321f30040ec49f8ea
youngmin <youngmin@youngminui-MacBookPro.local>
2024-07-31 17:10:15 +0900

    KL-123/feat : change of method to from method
    정적 팩토리 매서드를 of에서 from으로 바꿔 객체를 받아오도록 변경.

--- src/main/java/taco/klkl/domain/category/dto/response/CategoryResponseDto.java
+++ src/main/java/taco/klkl/domain/category/dto/response/CategoryResponseDto.java
@@ -4,7 +4,7 @@ import taco.klkl.domain.category.domain.CategoryName;
 
 public record CategoryResponseDto(
 	Long id,
-	String name
+	String category
 ) {
 	public static CategoryResponseDto of(Long id, CategoryName categoryName) {
 		return new CategoryResponseDto(id, categoryName.getName());
