a2165931bc2449e609ef254748ab74659a5b52e6
idealflower-k <aekrcu@naver.com>
2024-08-01 16:33:09 +0900

    KL-84/feat: create country dto for cities

---
+++ src/main/java/taco/klkl/domain/region/dto/response/CountryWithCitiesResponseDto.java
@@ -0,0 +1,27 @@
+package taco.klkl.domain.region.dto.response;
+
+import java.util.List;
+
+import taco.klkl.domain.region.domain.Country;
+
+public record CountryWithCitiesResponseDto(
+	CountryWithOutRegionDto country,
+	List<CityResponseDto> cities
+) {
+	public static CountryWithCitiesResponseDto from(Country country) {
+		return new CountryWithCitiesResponseDto(
+			CountryWithOutRegionDto.from(country),
+			country.getCities().stream()
+				.map(CityResponseDto::from)
+				.toList()
+		);
+	}
+
+	@Override
+	public String toString() {
+		return "CountryWithCitiesResponseDto{"
+			+ "country=" + country
+			+ ", cities=" + cities
+			+ '}';
+	}
+}
