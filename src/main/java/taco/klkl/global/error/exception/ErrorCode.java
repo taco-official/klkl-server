4c44f182e197c83b62c6ee5df8bf4e0677219008
idealflower-k <aekrcu@naver.com>
2024-08-01 18:22:39 +0900

    KL-126/feat: add currency not found exception

--- src/main/java/taco/klkl/global/error/exception/ErrorCode.java
+++ src/main/java/taco/klkl/global/error/exception/ErrorCode.java
@@ -27,7 +27,7 @@ public enum ErrorCode {
 	REGION_NOT_FOUND(HttpStatus.NOT_FOUND, "C050", "해당 지역을 찾을 수 없습니다."),
 	COUNTRY_NOT_FOUND(HttpStatus.NOT_FOUND, "C051", "해당 국가를 찾을 수 없습니다."),
 	CITY_NOT_FOUND(HttpStatus.NOT_FOUND, "C052", "해당 도시를 찾을 수 없습니다."),
-
+	CURRENCY_NOT_FOUND(HttpStatus.NOT_FOUND, "C053", "해당 통화를 찾을 수 없습니다."),
 	// Category
 	CATEGORY_ID_NOT_FOUND(HttpStatus.NOT_FOUND, "C060", "존재하지 않는 카테고리 ID 입니다."),
 	// Filter
