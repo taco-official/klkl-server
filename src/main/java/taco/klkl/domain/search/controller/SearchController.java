aed39ce25a786eecdef3db0cd1548b54fe7c360b
idealflower-k <aekrcu@naver.com>
2024-08-02 18:54:28 +0900

    KL-127/feat: create search controller

---
+++ src/main/java/taco/klkl/domain/search/controller/SearchController.java
@@ -0,0 +1,28 @@
+package taco.klkl.domain.search.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import taco.klkl.domain.search.dto.response.SearchResponseDto;
+import taco.klkl.domain.search.service.SearchService;
+
+@Slf4j
+@RestController
+@RequestMapping("/v1/search")
+@RequiredArgsConstructor
+@Tag(name = "6. 검색", description = "지역 관련 API")
+public class SearchController {
+
+	private final SearchService searchService;
+
+	@GetMapping()
+	public SearchResponseDto getSearchByQuery(@RequestParam(value = "q") String query) {
+		return searchService.getSearchResult(query);
+	}
+
+}
