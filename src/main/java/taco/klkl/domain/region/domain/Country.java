a8afbd3efde2f71e30ad6644e2c862923564b7c2
idealflower-k <aekrcu@naver.com>
2024-08-02 18:50:20 +0900

    KL-127/refactor: add lazy fetch option

--- src/main/java/taco/klkl/domain/region/domain/Country.java
+++ src/main/java/taco/klkl/domain/region/domain/Country.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -25,7 +26,7 @@ public class Country {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long countryId;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(
 		name = "region_id",
 		nullable = false
@@ -53,7 +54,7 @@ public class Country {
 	)
 	private String photo;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(
 		name = "currency_id",
 		nullable = false
