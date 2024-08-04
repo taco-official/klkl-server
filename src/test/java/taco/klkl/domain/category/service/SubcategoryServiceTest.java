ee2e505b8540015253995223015aae5bf9e47986
youngmin <youngmin@youngminui-MacBookPro.local>
2024-08-04 17:07:34 +0900

    KL-122/test : add Subcategory service test
    SubcategoryService가 잘 작동하는지 확인하는 테스트 코드 작성.

---
+++ src/test/java/taco/klkl/domain/category/service/SubcategoryServiceTest.java
@@ -0,0 +1,90 @@
+package taco.klkl.domain.category.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import taco.klkl.domain.category.dao.SubcategoryRepository;
+import taco.klkl.domain.category.domain.Category;
+import taco.klkl.domain.category.domain.CategoryName;
+import taco.klkl.domain.category.domain.Subcategory;
+import taco.klkl.domain.category.domain.SubcategoryName;
+import taco.klkl.domain.category.exception.SubcategoryNotFoundException;
+
+@ExtendWith(MockitoExtension.class)
+@Transactional
+public class SubcategoryServiceTest {
+	@Mock
+	private SubcategoryRepository subcategoryRepository;
+
+	@InjectMocks
+	private SubcategoryService subcategoryService;
+
+	private final Category category = Category.of(CategoryName.FOOD);
+	private final Subcategory subcategory1 = Subcategory.of(category, SubcategoryName.SNACK);
+	private final Subcategory subcategory2 = Subcategory.of(category, SubcategoryName.BEVERAGE);
+	private final Subcategory subcategory3 = Subcategory.of(category, SubcategoryName.DRINKS);
+
+	@Test
+	@DisplayName("Query id가 Long type으로 입력되고 subcategory가 존재하는 경우")
+	public void testGetSubcategoryListWithExistIds() {
+		//given
+		final List<String> subcategories = Arrays.asList("1", "2", "3");
+		final List<Long> subcategoriesIds = Arrays.asList(1L, 2L, 3L);
+		final List<Subcategory> subcategoriesList = Arrays.asList(subcategory1, subcategory2, subcategory3);
+
+		when(subcategoryRepository.findAllById(subcategoriesIds)).thenReturn(subcategoriesList);
+
+		//when
+		List<Subcategory> result = subcategoryService.getSubcategoryList(subcategories);
+
+		//then
+		assertNotNull(result);
+		assertEquals(3, result.size());
+
+		assertEquals(SubcategoryName.SNACK.getKoreanName(), result.get(0).getName().getKoreanName());
+		assertEquals(SubcategoryName.BEVERAGE.getKoreanName(), result.get(1).getName().getKoreanName());
+		assertEquals(SubcategoryName.DRINKS.getKoreanName(), result.get(2).getName().getKoreanName());
+
+		verify(subcategoryRepository, times(1)).findAllById(subcategoriesIds);
+	}
+
+	@Test
+	@DisplayName("Query id가 Long type으로 입력되고 subcategory가 존재하지 않는 경우")
+	public void testGetSubcategoryListWithNotExistIds() {
+		//given
+		final List<String> subcategories = Arrays.asList("1", "2", "3");
+		final List<Long> subcategoriesIds = Arrays.asList(1L, 2L, 3L);
+
+		//when
+		when(subcategoryRepository.findAllById(subcategoriesIds)).thenThrow(SubcategoryNotFoundException.class);
+
+		//then
+		assertThrows(SubcategoryNotFoundException.class, () -> {
+			subcategoryService.getSubcategoryList(subcategories);
+		});
+		verify(subcategoryRepository, times(1)).findAllById(subcategoriesIds);
+	}
+
+	@Test
+	@DisplayName("Query id 중 Long type 이외의 값이 들어오는 경우")
+	public void testGetSubcategoryListWithInvalidQueryParameters() {
+		//given
+		final List<String> subcategories = Arrays.asList("1", "2", "youngmin");
+
+		//when & then
+		assertThrows(IllegalArgumentException.class, () -> {
+			subcategoryService.getSubcategoryList(subcategories);
+		});
+	}
+}
