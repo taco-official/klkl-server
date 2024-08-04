ce8d5e9498afeaa2d9eecfeae0b47424822ddac2
youngmin <youngmin@youngminui-MacBookPro.local>
2024-08-04 17:05:28 +0900

    KL-122/feat : add SubcategoryService
    파라미터 값을 변환하고 존재하는 Subcategory인지 판단하는 Subcategory service 작성.

--- src/main/java/taco/klkl/domain/category/service/SubcategoryService.java
+++ src/main/java/taco/klkl/domain/category/service/SubcategoryService.java
@@ -1,14 +1,43 @@
 package taco.klkl.domain.category.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import taco.klkl.domain.category.dao.SubcategoryRepository;
+import taco.klkl.domain.category.domain.Subcategory;
+import taco.klkl.domain.category.exception.SubcategoryNotFoundException;
 
 @Slf4j
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class SubcategoryService {
 	private final SubcategoryRepository subcategoryRepository;
+
+	public List<Subcategory> getSubcategoryList(List<String> subcategoryQuery) {
+		List<Long> subcategoryIdList = parseSubcategoryQueryDto(subcategoryQuery);
+		List<Subcategory> subcategories = subcategoryRepository.findAllById(subcategoryIdList);
+		validateSubcategories(subcategoryQuery.size(), subcategories.size());
+		return subcategories;
+	}
+
+	private List<Long> parseSubcategoryQueryDto(List<String> subcategoryQuery) {
+		try {
+			return subcategoryQuery.stream()
+				.map(Long::parseLong)
+				.toList();
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException("Invalid subcategory ID format: " + e.getMessage());
+		}
+	}
+
+	private void validateSubcategories(final int request, final int result) {
+		if (request != result) {
+			throw new SubcategoryNotFoundException();
+		}
+	}
 }
