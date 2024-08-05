b475a71a4efb55a425223d93fc304590df1955ca
idealflower-k <aekrcu@naver.com>
2024-08-05 15:37:03 +0900

    KL-127/refactor: remove product, add category, subcategory

--- src/main/java/taco/klkl/domain/search/dto/response/SearchResponseDto.java
+++ src/main/java/taco/klkl/domain/search/dto/response/SearchResponseDto.java
@@ -2,20 +2,23 @@ package taco.klkl.domain.search.dto.response;
 
 import java.util.List;
 
-import taco.klkl.domain.product.dto.response.ProductSimpleResponseDto;
+import taco.klkl.domain.category.dto.response.CategoryResponseDto;
+import taco.klkl.domain.category.dto.response.SubcategoryResponseDto;
 import taco.klkl.domain.region.dto.response.CityResponseDto;
 import taco.klkl.domain.region.dto.response.CountrySimpleResponseDto;
 
 public record SearchResponseDto(
 	List<CountrySimpleResponseDto> countries,
 	List<CityResponseDto> cities,
-	List<ProductSimpleResponseDto> products
+	List<CategoryResponseDto> categories,
+	List<SubcategoryResponseDto> subcategories
 ) {
 	public static SearchResponseDto of(
 		List<CountrySimpleResponseDto> countries,
 		List<CityResponseDto> cities,
-		List<ProductSimpleResponseDto> products
+		List<CategoryResponseDto> categories,
+		List<SubcategoryResponseDto> subcategories
 	) {
-		return new SearchResponseDto(countries, cities, products);
+		return new SearchResponseDto(countries, cities, categories, subcategories);
 	}
 }
