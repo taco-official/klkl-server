900d2974f4af414f41c2dd7515cf018bc0fa7b5a
idealflower-k <aekrcu@naver.com>
2024-08-05 17:49:09 +0900

    KL-127/refactor: add valid in query param

--- src/main/java/taco/klkl/domain/search/controller/SearchController.java
+++ src/main/java/taco/klkl/domain/search/controller/SearchController.java
@@ -6,12 +6,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import taco.klkl.domain.search.dto.response.SearchResponseDto;
 import taco.klkl.domain.search.service.SearchService;
 
 @Slf4j
+@Valid
 @RestController
 @RequestMapping("/v1/search")
 @RequiredArgsConstructor
@@ -21,8 +24,12 @@ public class SearchController {
 	private final SearchService searchService;
 
 	@GetMapping()
-	public SearchResponseDto getSearchByQuery(@RequestParam(value = "q") String query) {
+	public SearchResponseDto getSearchByQuery(
+		@RequestParam(value = "q")
+		@NotBlank
+		String query
+	) {
+
 		return searchService.getSearchResult(query);
 	}
-
 }
