7296f62cb562ebd70c579a2216969f7b2f12dab2
idealflower-k <aekrcu@naver.com>
2024-08-05 15:36:17 +0900

    KL-127/feat: create get all subcategories by names

--- src/main/java/taco/klkl/domain/category/service/SubcategoryService.java
+++ src/main/java/taco/klkl/domain/category/service/SubcategoryService.java
@@ -1,14 +1,27 @@
 package taco.klkl.domain.category.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import taco.klkl.domain.category.dao.SubcategoryRepository;
+import taco.klkl.domain.category.domain.Subcategory;
+import taco.klkl.domain.category.domain.SubcategoryName;
+import taco.klkl.domain.category.dto.response.SubcategoryResponseDto;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class SubcategoryService {
 	private final SubcategoryRepository subcategoryRepository;
+
+	public List<SubcategoryResponseDto> getSubcategoriesBySubcategoryNames(List<SubcategoryName> subcategoryNames) {
+		List<Subcategory> findCategories = subcategoryRepository.findAllByNameIn(subcategoryNames);
+
+		return findCategories.stream()
+			.map(SubcategoryResponseDto::from)
+			.toList();
+	}
 }
