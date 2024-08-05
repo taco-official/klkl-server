c2957a1af708ffe1733275574d3f931e9dc315c0
idealflower-k <aekrcu@naver.com>
2024-08-05 15:35:28 +0900

    KL-127/feat: create get all cities by types

--- src/main/java/taco/klkl/domain/region/service/CityService.java
+++ src/main/java/taco/klkl/domain/region/service/CityService.java
@@ -1,8 +1,15 @@
 package taco.klkl.domain.region.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import taco.klkl.domain.region.dto.response.CityResponseDto;
+import taco.klkl.domain.region.enums.CityType;
 
 @Service
 public interface CityService {
 	boolean existsCityById(final Long id);
+
+	List<CityResponseDto> getAllCitiesByCityTypes(final List<CityType> cityTypes);
 }
--- src/main/java/taco/klkl/domain/region/service/CityServiceImpl.java
+++ src/main/java/taco/klkl/domain/region/service/CityServiceImpl.java
@@ -1,5 +1,7 @@
 package taco.klkl.domain.region.service;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -7,6 +9,9 @@ import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import taco.klkl.domain.region.dao.CityRepository;
+import taco.klkl.domain.region.domain.City;
+import taco.klkl.domain.region.dto.response.CityResponseDto;
+import taco.klkl.domain.region.enums.CityType;
 
 @Slf4j
 @Primary
@@ -20,5 +25,15 @@ public class CityServiceImpl implements CityService {
 	@Override
 	public boolean existsCityById(Long id) {
 		return cityRepository.existsById(id);
+	}
+
+	@Override
+	public List<CityResponseDto> getAllCitiesByCityTypes(List<CityType> cityTypes) {
+
+		List<City> findCities = cityRepository.findAllByNameIn(cityTypes);
+
+		return findCities.stream()
+			.map(CityResponseDto::from)
+			.toList();
 	}
 }
