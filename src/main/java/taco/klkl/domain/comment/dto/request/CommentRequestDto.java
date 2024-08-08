a06c30d62e717b278eaa61838b47b9897d809fb1
youngmin <youngmin@youngminui-MacBookPro.local>
2024-08-08 17:56:09 +0900

    KL-121/refactor : add validation message
    validation 메세지 추가.

--- src/main/java/taco/klkl/domain/comment/dto/request/CommentRequestDto.java
+++ src/main/java/taco/klkl/domain/comment/dto/request/CommentRequestDto.java
@@ -7,13 +7,13 @@ import taco.klkl.domain.comment.annotation.ExistUser;
 import taco.klkl.global.common.constants.CommentValidationMessage;
 
 public record CommentRequestDto(
-	@NotNull
+	@NotNull(message = CommentValidationMessage.USER_NOT_NULL)
 	@ExistUser(message = CommentValidationMessage.USER_NOT_FOUND)
 	Long userId,
 
-	@NotNull
-	@NotBlank
-	@Size(max = 400)
+	@NotNull(message = CommentValidationMessage.CONTENT_NOT_NULL)
+	@NotBlank(message = CommentValidationMessage.CONTENT_NOT_BLANK)
+	@Size(max = 400, message = CommentValidationMessage.CONTENT_SIZE)
 	String content
 ) {
 }
