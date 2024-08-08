397fd521dbdff6054739ec1de70ec0ff90f950c4
youngmin <youngmin@youngminui-MacBookPro.local>
2024-08-08 17:56:56 +0900

    KL-121/refactor : add validation messages
    validation 메세지들 추가.

--- src/main/java/taco/klkl/global/common/constants/CommentValidationMessage.java
+++ src/main/java/taco/klkl/global/common/constants/CommentValidationMessage.java
@@ -1,5 +1,9 @@
 package taco.klkl.global.common.constants;
 
 public final class CommentValidationMessage {
-	public static final String USER_NOT_FOUND = "존재하지 않는 유저입니다";
+	public static final String USER_NOT_NULL = "유저 ID는 필수 항목입니다.";
+	public static final String USER_NOT_FOUND = "존재하지 않는 유저입니다.";
+	public static final String CONTENT_NOT_NULL = "댓글 내용은 필수 항목입니다.";
+	public static final String CONTENT_NOT_BLANK = "댓글 내용은 비어있을 수 없습니다.";
+	public static final String CONTENT_SIZE = "댓글 내용은 400자 이하여야 합니다.";
 }
