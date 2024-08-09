be816e27e9995ad0e111d7afa8a2c2b059d43a84
youngmin <youngmin@youngminui-MacBookPro.local>
2024-08-09 22:06:41 +0900

    KL-121/refactor : refactor code value

--- src/main/java/taco/klkl/global/error/exception/ErrorCode.java
+++ src/main/java/taco/klkl/global/error/exception/ErrorCode.java
@@ -9,11 +9,11 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
 	// Common
-	METHOD_ARGUMENT_INVALID(HttpStatus.BAD_REQUEST, "C010", "유효하지 않은 method 인자 입니다."),
-	METHOD_NOT_SUPPORTED(HttpStatus.METHOD_NOT_ALLOWED, "C011", "지원하지 않는 HTTP method 입니다."),
-	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "C012", "서버에 문제가 발생했습니다. 관리자에게 문의해주세요."),
-	HTTP_MESSAGE_NOT_READABLE(HttpStatus.BAD_REQUEST, "C013", "잘못된 요청 메시지 형식입니다."),
-	QUERY_TYPE_MISMATCH(HttpStatus.BAD_REQUEST, "C014", "올바르지 않은 쿼리 타입 입니다."),
+	METHOD_ARGUMENT_INVALID(HttpStatus.BAD_REQUEST, "C001", "유효하지 않은 method 인자 입니다."),
+	METHOD_NOT_SUPPORTED(HttpStatus.METHOD_NOT_ALLOWED, "C002", "지원하지 않는 HTTP method 입니다."),
+	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "C003", "서버에 문제가 발생했습니다. 관리자에게 문의해주세요."),
+	HTTP_MESSAGE_NOT_READABLE(HttpStatus.BAD_REQUEST, "C004", "잘못된 요청 메시지 형식입니다."),
+	QUERY_TYPE_MISMATCH(HttpStatus.BAD_REQUEST, "C005", "올바르지 않은 쿼리 타입 입니다."),
 
 	// User
 
@@ -24,12 +24,17 @@ public enum ErrorCode {
 
 	// Comment
 	COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "C040", "존재하지 않는 댓글입니다."),
+	COMMENT_PRODUCT_NOT_MATCH(HttpStatus.BAD_REQUEST, "C041", "다른 상품에 있는 댓글입니다."),
 
 	// Region
 	REGION_NOT_FOUND(HttpStatus.NOT_FOUND, "C050", "해당 지역을 찾을 수 없습니다."),
+
 	COUNTRY_NOT_FOUND(HttpStatus.NOT_FOUND, "C051", "해당 국가를 찾을 수 없습니다."),
+
 	CITY_NOT_FOUND(HttpStatus.NOT_FOUND, "C052", "해당 도시를 찾을 수 없습니다."),
+
 	CURRENCY_NOT_FOUND(HttpStatus.NOT_FOUND, "C053", "해당 통화를 찾을 수 없습니다."),
+
 	// Category
 	CATEGORY_ID_NOT_FOUND(HttpStatus.NOT_FOUND, "C060", "존재하지 않는 카테고리 ID 입니다."),
 
