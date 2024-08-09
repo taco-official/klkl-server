02d2c152edfaef36d8c0d48352c786a455aefdbc
youngmin <youngmin@youngminui-MacBookPro.local>
2024-08-09 22:10:07 +0900

    KL-121/refactor : change Jpa method

--- src/main/java/taco/klkl/domain/comment/dao/CommentRepository.java
+++ src/main/java/taco/klkl/domain/comment/dao/CommentRepository.java
@@ -1,14 +1,11 @@
 package taco.klkl.domain.comment.dao;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import taco.klkl.domain.comment.domain.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-	List<Comment> findAllByProductId(Long productId);
-
-	Optional<Comment> findByProductId(Long productId);
+	List<Comment> findAllByProduct_ProductId(Long productId);
 }
