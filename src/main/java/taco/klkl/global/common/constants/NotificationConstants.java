6a4bcd8177b2f147b34cb726a575b8a694554699
youngmin <youngmin@youngminui-MacBookPro.local>
2024-08-12 14:39:41 +0900

    KL-134/feat : add Notification constants

---
+++ src/main/java/taco/klkl/global/common/constants/NotificationConstants.java
@@ -0,0 +1,8 @@
+package taco.klkl.global.common.constants;
+
+public class NotificationConstants {
+	public static final String DEFAULT_IS_READ_STRING = "false";
+	public static final boolean DEFAULT_IS_READ_VALUE = false;
+
+	public static final boolean UPDATED_IS_READ_VALUE = true;
+}
