61383fafbc1213c54384881f1157c3d250612674
youngmin <youngmin@youngminui-MacBookPro.local>
2024-08-13 15:02:36 +0900

    KL-134/refactor : add pagination logic.

--- src/main/java/taco/klkl/domain/notification/controller/NotificationController.java
+++ src/main/java/taco/klkl/domain/notification/controller/NotificationController.java
@@ -2,36 +2,47 @@ package taco.klkl.domain.notification.controller;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import taco.klkl.domain.notification.dto.response.NotificationResponse;
 import taco.klkl.domain.notification.service.NotificationService;
+import taco.klkl.global.common.constants.NotificationConstants;
+import taco.klkl.global.common.response.PagedResponseDto;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/notifications")
+@Tag(name = "7. 알림", description = "알림관련 API")
 public class NotificationController {
 
 	private final NotificationService notificationService;
 
 	@GetMapping
-	public List<NotificationResponse> getAllNotifications() {
-		return notificationService.getNotifications();
+	@Operation(summary = "알림 목록 조회", description = "알림 전체 목록을 조회합니다.")
+	public PagedResponseDto<NotificationResponse> getAllNotifications(
+		@PageableDefault(size = NotificationConstants.DEFAULT_PAGE_SIZE) Pageable pageable) {
+		return notificationService.getNotifications(pageable);
 	}
 
 	@PutMapping("/all")
+	@Operation(summary = "모든 알림 읽음", description = "모든 알림을 읽음으로 처리합니다.")
 	public List<NotificationResponse> readAllNotifications() {
 		return notificationService.readAllNotifications();
 	}
 
 	@PutMapping("/{notificationId}")
+	@Operation(summary = "단일 알림 읽음", description = "단일 알림을 읽음으로 처리합니다.")
 	public NotificationResponse readNotification(@PathVariable Long notificationId) {
 		return notificationService.readNotificationById(notificationId);
 	}
