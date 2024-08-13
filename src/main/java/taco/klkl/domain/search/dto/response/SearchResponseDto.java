789c71961b9f01aa168d554f2cefe753b413bb1a
idealflower-k <aekrcu@naver.com>
2024-08-13 17:10:02 +0900

    KL-140/refactor: add product dto

--- src/main/java/taco/klkl/domain/search/dto/response/SearchResponseDto.java
+++ src/main/java/taco/klkl/domain/search/dto/response/SearchResponseDto.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import taco.klkl.domain.category.dto.response.CategoryResponseDto;
 import taco.klkl.domain.category.dto.response.SubcategoryResponseDto;
+import taco.klkl.domain.product.dto.response.ProductSimpleResponseDto;
 import taco.klkl.domain.region.dto.response.CityResponseDto;
 import taco.klkl.domain.region.dto.response.CountrySimpleResponseDto;
 
@@ -11,14 +12,16 @@ public record SearchResponseDto(
 	List<CountrySimpleResponseDto> countries,
 	List<CityResponseDto> cities,
 	List<CategoryResponseDto> categories,
-	List<SubcategoryResponseDto> subcategories
+	List<SubcategoryResponseDto> subcategories,
+	List<ProductSimpleResponseDto> products
 ) {
 	public static SearchResponseDto of(
 		List<CountrySimpleResponseDto> countries,
 		List<CityResponseDto> cities,
 		List<CategoryResponseDto> categories,
-		List<SubcategoryResponseDto> subcategories
+		List<SubcategoryResponseDto> subcategories,
+		List<ProductSimpleResponseDto> products
 	) {
-		return new SearchResponseDto(countries, cities, categories, subcategories);
+		return new SearchResponseDto(countries, cities, categories, subcategories, products);
 	}
 }
