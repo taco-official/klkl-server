4019b91b1246efa5598326c654249139258a1f07
ohhamma <judyham10634@naver.com>
2024-08-15 16:22:52 +0900

    KL-131/feat: add Rating enum
    - 평점에 대한 enum 클래스 추가
    - 관련 예외 생성

---
+++ src/main/java/taco/klkl/domain/product/domain/Rating.java
@@ -0,0 +1,34 @@
+package taco.klkl.domain.product.domain;
+
+import java.util.Arrays;
+
+import lombok.Getter;
+import taco.klkl.domain.product.exception.RatingNotFoundException;
+
+@Getter
+public enum Rating {
+	ZERO_FIVE(0.5),
+	ONE(1.0),
+	ONE_FIVE(1.5),
+	TWO(2.0),
+	TWO_FIVE(2.5),
+	THREE(3.0),
+	THREE_FIVE(3.5),
+	FOUR(4.0),
+	FOUR_FIVE(4.5),
+	FIVE(5.0),
+	;
+
+	private final double value;
+
+	Rating(double value) {
+		this.value = value;
+	}
+
+	public static Rating from(final double value) {
+		return Arrays.stream(Rating.values())
+			.filter(r -> r.value == value)
+			.findFirst()
+			.orElseThrow(RatingNotFoundException::new);
+	}
+}
