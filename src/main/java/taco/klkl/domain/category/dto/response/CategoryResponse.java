c28191b36c54b65a24763ba79f312c8dfdcae18b
youngmin <youngmin@youngminui-MacBookPro.local>
2024-08-22 14:40:18 +0900

    KL-156/refactor: add subcategories.

--- src/main/java/taco/klkl/domain/category/dto/response/CategoryResponse.java
+++ src/main/java/taco/klkl/domain/category/dto/response/CategoryResponse.java
@@ -1,12 +1,21 @@
 package taco.klkl.domain.category.dto.response;
 
+import java.util.List;
+
 import taco.klkl.domain.category.domain.Category;
 
 public record CategoryResponse(
 	Long id,
-	String name
+	String name,
+	List<SubcategoryResponse> subcategory
 ) {
 	public static CategoryResponse from(final Category category) {
-		return new CategoryResponse(category.getId(), category.getName().getKoreanName());
+		return new CategoryResponse(
+			category.getId(),
+			category.getName().getKoreanName(),
+			category.getSubcategories().stream()
+				.map(SubcategoryResponse::from)
+				.toList()
+		);
 	}
 }
