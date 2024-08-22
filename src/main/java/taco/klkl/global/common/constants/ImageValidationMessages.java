ac6c8f32bfae89124892c5e37524eb4d125faf60
ohhamma <judyham10634@naver.com>
2024-08-22 20:01:51 +0900

    KL-119/feat: add ImageCreateRequest

---
+++ src/main/java/taco/klkl/global/common/constants/ImageValidationMessages.java
@@ -0,0 +1,7 @@
+package taco.klkl.global.common.constants;
+
+public final class ImageValidationMessages {
+	public static final String IMAGE_FILE_EXTENSION_NOT_NULL = "이미지 파일의 확장자는 필수 항목입니다.";
+
+	private ImageValidationMessages() {}
+}
