e99e83fd858add9981230c3c96201b793b8e116f
ohhamma <judyham10634@naver.com>
2024-08-27 16:05:06 +0900

    KL-119/rename: change user image upload request name

--- src/main/java/taco/klkl/domain/image/dto/request/UserImageUploadRequest.java
+++ src/main/java/taco/klkl/domain/image/dto/request/UserImageUploadRequest.java
@@ -3,7 +3,7 @@ package taco.klkl.domain.image.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import taco.klkl.global.common.constants.ImageValidationMessages;
 
-public record UserProfileImageUploadRequest(
+public record UserImageUploadRequest(
 	@NotBlank(message = ImageValidationMessages.FILE_EXTENSION_NOT_BLANK)
 	String fileExtension
 ) {
