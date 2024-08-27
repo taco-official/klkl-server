e6f8a8aa22e4689993bd506257853a62e69dd52c
ohhamma <judyham10634@naver.com>
2024-08-27 15:59:32 +0900

    KL-119/refactor: change user ImageType

--- src/main/java/taco/klkl/domain/image/domain/ImageType.java
+++ src/main/java/taco/klkl/domain/image/domain/ImageType.java
@@ -9,13 +9,13 @@ import taco.klkl.domain.image.exception.ImageTypeNotFoundException;
 @Getter
 @RequiredArgsConstructor
 public enum ImageType {
-	USER_PROFILE("user_profile"),
+	USER_IMAGE("user_image"),
 	PRODUCT_IMAGE("product_image"),
 	;
 
 	private final String value;
 
-	public static ImageType from(final String value) {
+	public static ImageType from(final String value) throws ImageTypeNotFoundException {
 		return Arrays.stream(ImageType.values())
 			.filter(type -> type.getValue().equals(value))
 			.findFirst()
