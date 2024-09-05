edf63801f9ce9bbf22209bb7edf5dd2901407e62
ohhamma <judyham10634@naver.com>
2024-09-05 19:08:08 +0900

    KL-165/feat: add single image request dtos

---
+++ src/main/java/taco/klkl/domain/image/dto/request/SingleImageUpdateRequest.java
@@ -0,0 +1,6 @@
+package taco.klkl.domain.image.dto.request;
+
+public record SingleImageUpdateRequest(
+	Long imageId
+) {
+}
--- src/main/java/taco/klkl/domain/image/dto/request/SingleImageUploadRequest.java
+++ src/main/java/taco/klkl/domain/image/dto/request/SingleImageUploadRequest.java
@@ -3,7 +3,7 @@ package taco.klkl.domain.image.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import taco.klkl.global.common.constants.ImageValidationMessages;
 
-public record ImageUploadRequest(
+public record SingleImageUploadRequest(
 	@NotBlank(message = ImageValidationMessages.FILE_EXTENSION_NOT_BLANK)
 	String fileExtension
 ) {
