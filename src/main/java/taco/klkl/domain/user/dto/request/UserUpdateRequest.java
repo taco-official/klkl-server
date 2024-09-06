5a15bf7a70ed839f9886bb581fec3bf37463b386
ohhamma <judyham10634@naver.com>
2024-09-06 18:22:00 +0900

    KL-166/refactor: add user validation messages

--- src/main/java/taco/klkl/domain/user/dto/request/UserUpdateRequest.java
+++ src/main/java/taco/klkl/domain/user/dto/request/UserUpdateRequest.java
@@ -1,12 +1,19 @@
 package taco.klkl.domain.user.dto.request;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.PositiveOrZero;
+import taco.klkl.global.common.constants.UserValidationMessages;
 
 public record UserUpdateRequest(
-	@NotNull(message = "이름은 필수 항목입니다.") String name,
-	@NotNull(message = "성별은 필수 항목입니다.") String gender,
-	@PositiveOrZero(message = "나이는 0 이상이어야 합니다.") Integer age,
+	@NotBlank(message = UserValidationMessages.NAME_NOT_BLANK)
+	String name,
+
+	@NotBlank(message = UserValidationMessages.GENDER_NOT_BLANK)
+	String gender,
+
+	@PositiveOrZero(message = UserValidationMessages.AGE_POSITIVE_OR_ZERO)
+	Integer age,
+
 	String description
 ) {
 }
