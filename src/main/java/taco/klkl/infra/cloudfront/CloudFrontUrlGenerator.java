40b48948c53994dd9bdb36a795cb7fd6136e2dd1
ohhamma <judyham10634@naver.com>
2024-09-06 15:46:09 +0900

    KL-165/feat: add CloudFrontUrlGenerator

---
+++ src/main/java/taco/klkl/infra/cloudfront/CloudFrontUrlGenerator.java
@@ -0,0 +1,23 @@
+package taco.klkl.infra.cloudfront;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import jakarta.annotation.PostConstruct;
+
+@Service
+public class CloudFrontUrlGenerator {
+	private static String cloudFrontDomain;
+
+	@Value("${cloud.aws.cloudfront.domain}")
+	private String injectedCloudFrontDomain;
+
+	@PostConstruct
+	private void init() {
+		cloudFrontDomain = injectedCloudFrontDomain;
+	}
+
+	public static String generateUrlByFileName(final String fileName) {
+		return "https://" + cloudFrontDomain + "/" + fileName;
+	}
+}
