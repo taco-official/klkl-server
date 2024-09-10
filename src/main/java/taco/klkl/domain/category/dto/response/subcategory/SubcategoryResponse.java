4080e234d910c0d25aef707775239d8022757ec7
ohhamma <judyham10634@naver.com>
2024-09-10 14:47:35 +0900

    KL-163/refactor: get tgas for subcategory response

--- src/main/java/taco/klkl/domain/category/dto/response/subcategory/SubcategoryResponse.java
+++ src/main/java/taco/klkl/domain/category/dto/response/subcategory/SubcategoryResponse.java
@@ -1,12 +1,26 @@
 package taco.klkl.domain.category.dto.response.subcategory;
 
+import java.util.List;
+
+import taco.klkl.domain.category.domain.SubcategoryTag;
 import taco.klkl.domain.category.domain.subcategory.Subcategory;
+import taco.klkl.domain.category.dto.response.tag.TagResponse;
 
 public record SubcategoryResponse(
 	Long id,
-	String name
+	String name,
+	List<TagResponse> tags
 ) {
 	public static SubcategoryResponse from(final Subcategory subcategory) {
-		return new SubcategoryResponse(subcategory.getId(), subcategory.getName());
+		final List<TagResponse> tags = subcategory.getSubcategoryTags().stream()
+			.map(SubcategoryTag::getTag)
+			.map(TagResponse::from)
+			.toList();
+
+		return new SubcategoryResponse(
+			subcategory.getId(),
+			subcategory.getName(),
+			tags
+		);
 	}
 }
