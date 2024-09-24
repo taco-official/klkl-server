7742379301e31fbd0e693111b968b4bfda04353f
ohhamma <judyham10634@naver.com>
2024-09-24 18:58:16 +0900

    KL-151/fix: checkstyle error

--- src/main/java/taco/klkl/domain/member/domain/Member.java
+++ src/main/java/taco/klkl/domain/member/domain/Member.java
@@ -2,6 +2,7 @@ package taco.klkl.domain.member.domain;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -9,7 +10,6 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
