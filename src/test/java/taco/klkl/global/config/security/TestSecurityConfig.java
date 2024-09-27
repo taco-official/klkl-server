5c7b3c489da1e3f4d96aee8077e449b79a7719b0
ohhamma <judyham10634@naver.com>
2024-09-27 16:35:56 +0900

    KL-151/test: add test security config

---
+++ src/test/java/taco/klkl/global/config/security/TestSecurityConfig.java
@@ -0,0 +1,22 @@
+package taco.klkl.global.config.security;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@TestConfiguration
+public class TestSecurityConfig {
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf(AbstractHttpConfigurer::disable)
+
+			.authorizeHttpRequests(authorizeRequests ->
+				authorizeRequests
+					.anyRequest().permitAll());
+
+		return http.build();
+	}
+}
