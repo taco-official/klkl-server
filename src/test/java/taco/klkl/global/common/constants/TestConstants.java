92d632cbd5b261bfbcf68fa2e997269adef20262
ohhamma <judyham10634@naver.com>
2024-09-27 18:26:42 +0900

    KL-151/test: add test constants

---
+++ src/test/java/taco/klkl/global/common/constants/TestConstants.java
@@ -0,0 +1,9 @@
+package taco.klkl.global.common.constants;
+
+public final class TestConstants {
+
+	public static final String TEST_UUID = "550e8400-e29b-41d4-a716-446655440000";
+
+	private TestConstants() {
+	}
+}
